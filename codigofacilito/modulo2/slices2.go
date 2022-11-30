d1e2ab5e0335fed02a57afbde6f1b06724ca5439
Omar I. Martínez Gómez <omar_238@hotmail.com>
2022-11-30 13:01:58 -0600

    facilito -> empezando 2.6 función make

--- codigofacilito/modulo2/slices2.go
+++ codigofacilito/modulo2/slices2.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+/*
+Slices:  Se componen de los siguientes elementos
+1.- Un puntero
+2.- Una longitud
+3.- Una capacidad
+*/
+
 func main() {
 	meses := []string{"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
 		"Agosto", "Septiembre"} //,"Octubre","Noviembre","Diciembre"}
@@ -9,12 +16,19 @@ func main() {
 	longitud := len(meses)
 	capacidad := cap(meses)
 
-	fmt.Printf("La longitud es: %v, La capacidad es: %v\n", longitud, capacidad)
-	
+	fmt.Printf("La longitud es: %v, La capacidad es: %v %p\n", longitud, capacidad, meses)
+
+	// Si la estructura se encuentra en su capacidad máxima
 	meses = append(meses, "Octubre")
 	longitud = len(meses)
 	capacidad = cap(meses)
 	
-	fmt.Printf("La longitud es: %v, La capacidad es: %v\n", longitud, capacidad)
-
+	fmt.Printf("La longitud es: %v, La capacidad es: %v %p\n", longitud, capacidad, meses)
+	
+	meses = append(meses, "Noviembre")
+	meses = append(meses, "Diciembre")
+	longitud = len(meses)
+	capacidad = cap(meses)
+	
+	fmt.Printf("La longitud es: %v, La capacidad es: %v %p\n", longitud, capacidad, meses)
 }
