d92510bad19a3acd39ff4f2fbc1b8e9698e67cf8
Omar I. Martínez Gómez <omar_238@hotmail.com>
2022-12-29 22:59:05 -0600

    facilito -> 3.10 panic incompleto

--- codigofacilito/modulo3/foreach.go
+++ codigofacilito/modulo3/foreach.go
@@ -4,17 +4,25 @@ import (
 	"fmt"
 )
 
-func main()  {
-	animales := [...]string{"Perro","Gato","Pez","Vaca","Cerdo"}
+func main() {
+	animales := [...]string{"Perro", "Gato", "Pez", "Vaca", "Cerdo"}
 
 	/*
-	for indice := 0; indice < len(animales); indice++{
-		elemento := animales[indice]
-		fmt.Println(elemento)
-	}
+		for indice := 0; indice < len(animales); indice++{
+			elemento := animales[indice]
+			fmt.Println(elemento)
+		}
 	*/
-
+	
+	// foreach con dos variables
 	for indice, elemento := range animales {
-		fmt.Println("El indice es:",indice,", el valor es:",elemento)
+		fmt.Println("El indice es:", indice, ", el valor es:", elemento)
+	}
+
+	fmt.Println()
+
+	// foreach con una variable
+	for _, elemento := range animales {
+		fmt.Println("El valor es:", elemento)
 	}
 }
---
+++ codigofacilito/modulo3/panic.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main()  {
+	// funcion panic
+
+	var dividendo, divisor int
+
+	fmt.Print("ingresa un valor para el dividendo: ")
+	fmt.Scanf("%d",&dividendo)
+
+	fmt.Print()
+	fmt.Scanf("%d",divisor)
+}
