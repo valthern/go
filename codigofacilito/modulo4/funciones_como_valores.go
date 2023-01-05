a35918785001bc13d7f98ec8390d412d2d1842a6
Omar I. Martínez Gómez <omar_238@hotmail.com>
2023-01-05 13:59:39 -0600

    facilito -> 4.11 empezando funciones como valores ..

--- codigofacilito/modulo4/funciones_como_valores.go
+++ codigofacilito/modulo4/funciones_como_valores.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
-func main()  {
-	
+func factorial(numero int) int {
+	if numero == 1 || numero == 0 {
+		return 1
+	}
+
+	return factorial(numero-1) * numero
+}
+
+func main() {
+	var miFuncion = factorial
+
+	resultado := miFuncion(3)
+
+	fmt.Println(resultado)
 }
