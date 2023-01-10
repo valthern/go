89e34116cc42d01abc49dec97283f7ab93bb3ea5
Omar I. Martínez Gómez <omar_238@hotmail.com>
2023-01-10 17:20:19 -0600

    facilito -> 4.16 funcion recover terminada

--- codigofacilito/modulo4/programar_funciones.go
+++ codigofacilito/modulo4/programar_funciones.go
@@ -3,20 +3,21 @@ package main
 import "fmt"
 import "os"
 
-func main()  {
+func main() {
 	file, err := os.Open("example.txt")
 
-	if err != nil{
+	if err != nil {
 		panic("No fue posible obtener el archivo!")
 	}
-	
-	defer func ()  {
+
+	defer func() {
 		fmt.Println("Cerramos el archivo!")
 		file.Close()
-	}
+	}()
 
-	contenido := make([]byte,254)
+	contenido := make([]byte, 254)
 	longitud, _ := file.Read(contenido)
-
 	contenidoArchivo := string(contenido[0:longitud])
+
+	fmt.Println(contenidoArchivo)
 }
