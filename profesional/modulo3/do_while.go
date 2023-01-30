1eed384f537045b1994d5c15ab0f88fede818fef
Omar I. Martínez Gómez <omar_238@hotmail.com>
2023-01-29 22:16:27 -0600

    youtube -> 18, Channels, comenzando

--- profesional/modulo3/do_while.go
+++ profesional/modulo3/do_while.go
@@ -5,6 +5,7 @@ import "fmt"
 func main() {
 	var numero = 10
 
+	// Variable booleana, siempre "ok", y luego la condición asignada a la variable ok
 	for ok := true; ok; ok = numero < 10 {
 		fmt.Println(numero)
 		numero++
--- youtube/goroutines.go
+++ youtube/goroutines.go
@@ -7,9 +7,9 @@ import(
 )
 
 func main()  {
-	go mi_nombre_lentooo("Omar")
+	go mi_nombre_lentooo("Estereotipo")
 	fmt.Println("Queeee aburridoooooo")
-
+	
 	go func ()  {	
 		var wait string
 		fmt.Scanln(&wait)
@@ -20,7 +20,7 @@ func mi_nombre_lentooo(name string)  {
 	letras := strings.Split(name,"")
 
 	for _,letra := range(letras){
-		time.Sleep(1 * time.Second)
+		time.Sleep(time.Second)
 		fmt.Println(letra)
 	}
 }
