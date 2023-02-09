a6c643572b1a3f0d14d629a547f2ecdd1a76b09f
Omar I. Martínez Gómez <omar_238@hotmail.com>
2023-02-08 18:05:58 -0600

    youtube -> manejo de archivos 1 terminado, empezando con clase 2

--- youtube/io_read.go
+++ youtube/io_read.go
@@ -6,5 +6,11 @@ import (
 )
 
 func main()  {
-	ioutil.ReadFile()
+	file_data,err := ioutil.ReadFile("./hola.txt")
+
+	if err != nil {
+		fmt.Println("Hubo un error al tratar de leer el archivo.")
+	}
+
+	fmt.Println(string(file_data))
 }
