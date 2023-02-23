153d004b6d7a94fb68b3aa3ee274650d8574ccac
Omar I. Martínez Gómez <omar_238@hotmail.com>
2023-02-22 21:29:49 -0600

    youyube -> 26 Creacion de paquetes, empezando

--- youtube/json.go
+++ youtube/json.go
@@ -1,19 +1,26 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
-	//"encoding/json"
 )
 
-type Curso struct{
-	Title string
-	NumeroVideos int
+type Curso struct {
+	Title        string `json:"title"`
+	NumeroVideos int    `json:"numero_videos"`
 }
 
-func main()  {
-	http.HandleFunc("/",func (w http.ResponseWriter,r *http.Request)  {
-		
+type Cursos []Curso
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		cursos := Cursos{
+			Curso{"Curso de Go", 30},
+			Curso{"Curso de Ruby", 20},
+			Curso{"Curso de NodeJS", 40},
+		}
+		json.NewEncoder(w).Encode(cursos)
 	})
 
-	http.ListenAndServe(":8000",nil)
+	http.ListenAndServe(":8000", nil)
 }
