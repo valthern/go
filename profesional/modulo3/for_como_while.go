32b26e858b2077ac0c23cb9b18178e5e25b1476d
Omar I. Martínez Gómez <omar_238@hotmail.com>
2023-04-20 23:53:42 -0600

    profesional -> correcion de comentario en archivo while del modulo 3

--- profesional/modulo3/for_como_while.go
+++ profesional/modulo3/for_como_while.go
@@ -6,7 +6,7 @@ func main() {
 	numero := 12345
 	contador := 0
 
-	// Funciona como "while". Sólo contiene una parte que es la condicional
+	// Funciona como "while". Sólo contiene la condición. Se usa como un "while" tal cual.
 	for numero > 0 {
 		numero = numero / 10
 		contador++
