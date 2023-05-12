cb2dcb91ffd9f23c676377d1d98ac24c90783083
Omar I. Martínez Gómez <omar_238@hotmail.com>
2023-05-11 23:54:45 -0600

    edteam -> pasamos a la seccion 3 para empezarla

--- edteam/desdecero/main.go
+++ edteam/desdecero/main.go
@@ -8,12 +8,20 @@ import "fmt"
 func main() {
 	// dog es la primera variable de prueba
 	// cat es la segunda variable de prueba
-	dog, cat := "🐕", "😺"
+	dog, cat := "🐕", "😺" 
 	// face es la tercera variable de prueba
 	cat, face := "gatico", "🥴"
-
-	// Pi es una constante que vale 3.14159265358979...
-	const Pi = 3.14159265358979
-
 	fmt.Println(dog, cat, face)
+
+	// a es de tipo rune, el cual es int32 y tambien caracter unicode
+	var a rune = 97
+	fmt.Printf("Tipo: %T, Valor: %c\n", a, a)
+
+	var x uint8 = 100
+	var y uint16 = 23000
+	// z es casteada
+	z := uint16(x) + y
+	fmt.Printf("Tipo: %T, Valor: %v\n",z,z)
+	// Pi es una constante que vale 3.14159265358979...
+	//const Pi = 3.14159265358979
 }
