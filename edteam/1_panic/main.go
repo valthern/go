930f0c6a0688070877742718a8efdc4ae261d458
Omar I. Martínez Gómez <omar_238@hotmail.com>
2023-05-25 16:56:13 -0600

    edteam -> funciones panic y recover terminados

--- edteam/1_panic/main.go
+++ edteam/1_panic/main.go
@@ -3,18 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	division(10,2)
-	division(40,3)
-	division(12,0)
-	division(20,4)
+	division(10, 2)
+	division(40, 3)
+	division(12, 0)
+	division(20, 4)
 }
 
 func division(dividendo, divisor int) {
-	defer func ()  {
-		if r := recover(); r!=nil{
-			fmt.Println("Recuperandome del panic",r)
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recuperandome del panic", r)
 		}
-	}
+	}()
 	validarDivisor(divisor)
 	fmt.Println(dividendo / divisor)
 }
