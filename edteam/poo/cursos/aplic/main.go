9a72a8ed29b310f1d392806772739d7f3b9cefd3
Omar I. Martínez Gómez <omar_238@hotmail.com>
2023-08-03 23:50:52 -0600

    edteam -> poo 2, correccion

--- edteam/poo/cursos/aplic/main.go
+++ edteam/poo/cursos/aplic/main.go
@@ -8,8 +8,8 @@ import (
 
 func main() {
 	Go := course.NewCourse("Go desde Cero", 0, false)
-	Go.UserIDs: []uint{12, 56, 89}
-	Go.Classes: map[uint]string{
+	Go.UserIDs = []uint{12, 56, 89}
+	Go.Classes = map[uint]string{
 			1: "Introducción",
 			2: "Estructuras",
 			3: "Maps",
