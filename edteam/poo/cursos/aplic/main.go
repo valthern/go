1e28c94ade9b3f354114bcd7a46ba401a2ae19d0
Omar I. Martínez Gómez <omar_238@hotmail.com>
2023-08-03 23:48:44 -0600

    edteam -> funcion new como metodo constructor, incompleto

--- edteam/poo/cursos/aplic/main.go
+++ edteam/poo/cursos/aplic/main.go
@@ -7,19 +7,15 @@ import (
 )
 
 func main() {
-	Go := &course.Course {
-		Name: "Go desde Cero",
-		Price: 12.34,
-		IsFree: false,
-		UserIDs: []uint{12, 56, 89},
-		Classes: map[uint]string{
+	Go := course.NewCourse("Go desde Cero", 0, false)
+	Go.UserIDs: []uint{12, 56, 89}
+	Go.Classes: map[uint]string{
 			1: "Introducción",
 			2: "Estructuras",
 			3: "Maps",
-		},
+		}
 	}
 
 	Go.PrintClasses()
-	Go.ChangePrice(67.12)
 	fmt.Println(Go.Price)
 }
--- edteam/poo/cursos/course/course.go
+++ edteam/poo/cursos/course/course.go
@@ -4,19 +4,30 @@ import (
 	"fmt"
 )
 
-type Course struct {
+type course struct {
 	Name    string
 	Price   float64
 	IsFree  bool
 	UserIDs []uint
 	Classes map[uint]string
 }
+func NewCourse(name string, price float64, isFree bool) *course {
+	if price == 0 {
+		price = 30
+	}
 
-func (c *Course) ChangePrice(price float64)  {
+	return &course{
+		Name: name,
+		Price: price,
+		IsFree: isFree,
+	}
+}
+
+func (c *course) changePrice(price float64)  {
 	c.Price = price
 }
 
-func (c *Course) PrintClasses() {
+func (c *course) PrintClasses() {
 	text := "Las clases son: "
 	for _, class := range c.Classes {
 		text += class + ", "
