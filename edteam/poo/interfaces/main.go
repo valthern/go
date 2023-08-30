139c11bd9afc6fac33e5c0f7d6bae916b6093b43
Omar I. Martínez Gómez <omar_238@hotmail.com>
2023-08-29 23:01:26 -0600

    edteam -> poo 4.1 interfaces pt1 completada

--- edteam/poo/interfaces/main.go
+++ edteam/poo/interfaces/main.go
@@ -1,5 +1,27 @@
 package main
 
+import "fmt"
+
+type Greeter interface {
+	Greet()
+}
+
+type Person struct {
+	Name string
+}
+
+func (p Person) Greet() {
+	fmt.Printf("Hola, soy %s\n", p.Name)
+}
+
+type Text string
+
+func (t Text) Greet() {
+	fmt.Printf("Hola, soy %s\n", t)
+}
+
 func main()  {
-	
+	// var g Greeter = Person{ Name: "Alejandro"}
+	var g Greeter = Text("Daisy")
+	g.Greet()
 }
