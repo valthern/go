818aaad7d226dcc8800f66ab7af454e939612e9f
Omar I. Martínez Gómez <omar_238@hotmail.com>
2023-09-01 22:02:17 -0600

    edteam -> poo 4.3 Interfaces, terminado

--- edteam/poo/interfaces/main.go
+++ edteam/poo/interfaces/main.go
@@ -6,6 +6,15 @@ type Greeter interface {
 	Greet()
 }
 
+type Byer interface {
+	Bye()
+}
+
+type GreeterByer interface {
+	Greeter
+	Byer
+}
+
 type Person struct {
 	Name string
 }
@@ -14,23 +23,45 @@ func (p Person) Greet() {
 	fmt.Printf("Hola, soy %s\n", p.Name)
 }
 
+func (p Person) Bye() {
+	fmt.Printf("Adiós, soy %s\n", p.Name)
+}
+
 type Text string
 
 func (t Text) Greet() {
 	fmt.Printf("Hola, soy %s\n", t)
 }
 
-func GreetAll(gs ...Greeter)  {
-	for _, g := range gs {
-		g.Greet()
-		fmt.Printf("\t Valor: %v, Tipo: %T\n", g, g)
+func (t Text) Bye() {
+	fmt.Printf("Adiós, soy %s\n", t)
+}
+
+// func GreetAll(gs ...Greeter) {
+// 	for _, g := range gs {
+// 		g.Greet()
+// 		// fmt.Printf("\t Valor: %v, Tipo: %T\n", g, g)
+// 	}
+// }
+
+// func ByeAll(bs ...Byer) {
+// 	for _, b := range bs {
+// 		b.Bye()
+// 		// fmt.Printf("\t Valor: %v, Tipo: %T\n", b, b)
+// 	}
+// }
+
+func All(gbs ...GreeterByer) {
+	for _, gb := range gbs {
+		gb.Greet()
+		gb.Bye()
 	}
 }
 
-func main()  {
+func main() {
 	p := Person{Name: "Alejandro"}
 	var t Text = "Daisy"
 	// p.Greet()
 	// t.Greet()
-	GreetAll(p, t)
+	All(p, t)
 }
