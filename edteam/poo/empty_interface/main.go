25f7d6589b78e97bb9a2d99d9d3d958389c0121d
Omar I. Martínez Gómez <omar_238@hotmail.com>
2023-09-21 23:26:12 -0600

    edteam -> 4.8 type assertions, completado

--- edteam/poo/empty_interface/main.go
+++ edteam/poo/empty_interface/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type exampler interface {
 	x()
@@ -8,10 +11,16 @@ type exampler interface {
 
 func wrapper(i interface{}) {
 	fmt.Printf("valor: %v, Tipo: %T\n", i, i)
+
+	v, ok := i.(string)
+	if ok {
+		fmt.Printf("\t%s\n", strings.ToUpper(v))
+	}
 }
 
 func main() {
 	wrapper(12)
+	wrapper("AdeAdalberto")
 	wrapper(34.68595)
 	wrapper(false)
 	wrapper("Qué pedo")
