b32fba07211ad6ae7276bdd0989c7960cddf60f1
Omar I. Martínez Gómez <omar_238@hotmail.com>
2024-03-14 23:07:43 -0600

    platzi -> c 10, 2:10

--- src/curso_goland_platzi/src/main.go
+++ src/curso_goland_platzi/src/main.go
@@ -3,17 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	// Declaración de constantes. Hasta aquí llega la precisión del float64
-	const pi float64 = 3.141592653589793
-	const pi2 = 3.14
 
-	fmt.Println("pi:", pi)
-	fmt.Println("pi2:", pi2)
-
-	// Declaración de variables enteras
-	base := 12
-	var altura int = 14
-	var area int
-
-	fmt.Println(base, altura, area)
+	// Area cuadrado
+	const baseCuadrado = 10
+	areaCuadrado := baseCuadrado * baseCuadrado
+	fmt.Println("Area cuadrado:", areaCuadrado)
 }
