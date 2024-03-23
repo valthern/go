833b3392553d5ebf3e2c10c1e400566728de4820
Omar I. Martínez Gómez <omar_238@hotmail.com>
2024-03-22 23:28:29 -0600

    platzi -> c 18, terminado

--- src/curso_goland_platzi/src/main.go
+++ src/curso_goland_platzi/src/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"time"
+)
 
 func doubleReturn(a int) (c, d int) {
 	return a, a * 2
@@ -8,17 +13,12 @@ func doubleReturn(a int) (c, d int) {
 
 func main() {
 
-	// Area cuadrado
-	const baseCuadrado = 10
-	areaCuadrado := baseCuadrado * baseCuadrado
-	fmt.Println("Area cuadrado:", areaCuadrado)
-
-	//Tipo de dato
-	fmt.Printf("Tipo de datos de baseCuadrado: %T\n", baseCuadrado)
-
 	// Devolución de dos valores
 	value1, value2 := doubleReturn(8)
 	fmt.Println("Valor1: ", value1, " Valor2: ", value2)
+
+	// Tipo de dato
+	fmt.Printf("Tipo de datos de valor1: %T\n", value1)
 
 	// Ciclos
 	//For condicional
@@ -36,19 +36,72 @@ func main() {
 	fmt.Println()
 
 	// For de rango
-	entry := []string{"Jack","John","Jones"}
+	entry := []string{"Jack", "John", "Jones"}
 	for i, val := range entry {
 		fmt.Printf("At position %d, the character %s is present\n", i, val)
 	}
 
 	// For forever
-	counterForever := 0
-	for {
-		fmt.Println(counterForever)
-		counterForever++
-		if counterForever > 0 {
-			break;
-		}
+	// counterForever := 0
+	// for {
+	// 	fmt.Println(counterForever)
+	// 	counterForever++
+	// 	if counterForever > 0 {
+	// 		break
+	// 	}
+	// }
+
+	// if con 2 variables internas
+	if valueI, errI := strconv.Atoi("69"); errI != nil {
+		log.Fatal(errI)
+	} else {
+		fmt.Println("Valor I:", valueI)
 	}
 
+	// if con 2 varaiables externas
+	valueE, errE := strconv.Atoi("99")
+	if errE != nil {
+		log.Fatal(errE)
+	}
+	fmt.Println("Valor E:", valueE)
+
+	// switch para iterar sobre una variable
+	switch hora := time.Now().Hour(); hora {
+	case 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11:
+		fmt.Println("Buenos días. Son las", hora, "horas")
+	case 12, 13, 14, 15, 16, 17, 18:
+		fmt.Println("Buenas tardes. Son las", hora, "horas")
+	case 19, 20, 21, 22, 23:
+		fmt.Println("Buenas noches. Son las", hora, "horas")
+	}
+
+	// switch para iterar sobre evaluaciones con una variable
+	gradosCentigradosDelAgua := 80
+	switch {
+	case gradosCentigradosDelAgua >= 100:
+		fmt.Println("El agua está en estado gaseoso:", gradosCentigradosDelAgua, "°C")
+	case gradosCentigradosDelAgua <= 0:
+		fmt.Println("El agua está en estado sólido:", gradosCentigradosDelAgua, "°C")
+	default:
+		fmt.Println("El agua está en estado líquido:", gradosCentigradosDelAgua, "°C")
+	}
+
+	// Slice
+	slice := []int{0, 1, 2, 3, 4, 5, 6}
+	fmt.Println(slice, "Length Slice:", len(slice), "Capacity Slice:", cap(slice))
+
+	// Métodos en el slice (primer índice es inclusivo, el segudo es exclusivo)
+	fmt.Println(slice[0])
+	fmt.Println(slice[:3])
+	fmt.Println(slice[2:4])
+	fmt.Println(slice[4:])
+
+	// Append
+	slice = append(slice, 7)
+	fmt.Println(slice)
+
+	// Append nueva lista
+	newSlice := []int{8, 9, 10}
+	slice = append(slice, newSlice...)
+	fmt.Println(slice)
 }
